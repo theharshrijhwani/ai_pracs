cc212a4b4044a12a1711699c340255402af4bc6b
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2023-10-29 13:23:36 +0530

    added second dfs code

--- dfs.py
+++ dfs.py
@@ -1,24 +1,31 @@
-graph = {
-    "A": ["B", "D"],
-    "B": ["C", "A"],
-    "C": ["B", "D"],
-    "D": ["A", "C"],
-    "E": ["B", "F"],
-    "F": ["E", "G"],
-    "G": ["E"]
-}
-
-visited = set()
+from collections import defaultdict
 
 
-def dfs(visited, graph, node):
-    if node not in visited:
-        print(node)
-        visited.add(node)
-        for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+class Graph:
+    def __init__(self):
+        self.adj = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.adj[u].append(v)
+        self.adj[v].append(u)
+
+    def helper(self, u, visited):
+        visited.add(u)
+        print(u)
+        for v in self.adj[u]:
+            if (v not in visited):
+                self.helper(v, visited)
+
+    def dfs(self, u):
+        visited = set()
+        self.helper(u, visited)
 
 
-print("-------DFS-------")
+g = Graph()
 
-dfs(visited, graph, "E")
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(0, 3)
+g.add_edge(2, 4)
+
+g.dfs(4)
